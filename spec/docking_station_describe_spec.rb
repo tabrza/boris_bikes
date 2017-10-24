fdbd9a40737ed9d2421255651b3cdfd2c431f4b2
Tabish Raza <tabishraza@gmail.com>
2017-10-24 16:50:58 +0100

    isolated test problems for release and working of bike

--- spec/docking_station_describe_spec.rb
+++ spec/docking_station_describe_spec.rb
@@ -1,11 +1,16 @@
 require 'docking_station'
+require'bike'
 
 describe DockingStation do
 
 ds =   DockingStation.new
 
-  it "releases bike" do
-    expect(ds).to respond_to (:release_bike)
+  # it "releases bike" do
+  #   expect(ds).to respond_to (:release_bike)
+  # end
+  #&&
+  it "works" do
+    expect(ds.release_bike).to respond_to (:working?)
   end
 
 end
